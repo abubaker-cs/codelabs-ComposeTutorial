97cd8cbb85d21f4e7fc40aa104a5206f4da62faa
abubaker-cs <mohammad_abubaker@yahoo.com>
2023-01-01 02:54:42 +0500

    Implemented Dark Mode for the layout.

--- app/src/main/java/org/abubaker/composetutorial/MainActivity.kt
+++ app/src/main/java/org/abubaker/composetutorial/MainActivity.kt
@@ -1,14 +1,19 @@
 package org.abubaker.composetutorial
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,10 +35,15 @@ class MainActivity : ComponentActivity() {
         // The setContent block defines the activity's layout where composable functions are called.
         setContent {
 
+            // ComposeTutorialTheme is a wrapper around the Material Design theme.
+            // It is defined in the ui/theme.kt package.
             ComposeTutorialTheme {
+
+                // Surface is a container that allows us to apply a background color
                 Surface(modifier = Modifier.fillMaxSize()) {
                     MessageCard(Message("Android", "Jetpack Compose"))
                 }
+
             }
 
         }
@@ -69,10 +79,15 @@ class MainActivity : ComponentActivity() {
                 modifier = Modifier
 
                     // Padding: 8dp
-                    .padding(8.dp)
+                    // .padding(8.dp)
 
                     // Image size = 40dp
                     .size(40.dp)
+
+                    .clip(CircleShape)
+
+                    // Add a border around the image
+                    .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
 
             )
 
@@ -83,10 +98,25 @@ class MainActivity : ComponentActivity() {
             Column {
 
                 // Author?
-                Text(text = msg.author)
+                Text(
+                    text = msg.author,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle2
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
 
                 // Message?
-                Text(text = msg.body)
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    elevation = 1.dp
+                ) {
+                    Text(
+                        text = msg.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        style = MaterialTheme.typography.body2
+                    )
+                }
 
             }
         }
@@ -98,7 +128,12 @@ class MainActivity : ComponentActivity() {
      *
      * The annotation must be used on a composable function that does not take in parameters
      */
-    @Preview
+    @Preview(name = "Light Mode")
+    @Preview(
+        uiMode = UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode"
+    )
     @Composable
     fun PreviewMessageCard() {
         ComposeTutorialTheme {
